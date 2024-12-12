f6b982aeaa53fdae5abcd44e904183db3314fa73
THAKRE <vaibhavi-dilip.thakre@capgemini.com>
2024-12-12 09:34:13 +0530

    first commit

---
+++ 1A-EurekaServer/src/test/java/com/cg/_A_EurekaServer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg._A_EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
